8dcd9e72b1fa532f4edc02d548de6c1be3e631c8
Abhishek Gupta <abhishekgupt.in@gmail.com>
2021-03-30 04:54:39 +0530

    Completed JS logic, only CSS remains

--- app.js
+++ app.js
@@ -2,7 +2,18 @@ var translateBtn = document.querySelector("#btn-translate");
 var inputText = document.querySelector("#txt-input");
 var outputText = document.querySelector("#txt-output")
 
+var baseURL = "https://api.funtranslations.com/translate/minion.json";
+
+function errorHandler(error) {
+    console.log("Error: " + error);
+    alert("Something wrong with server, try again after some time!");
+}
+
 translateBtn.addEventListener("click", function translate() {
     var input = inputText.value;
-    outputText.innerText = input;
+    var url = baseURL + "?text=" + input;
+    fetch(url)
+        .then(resp => resp.json())
+        .then(json => outputText.innerText = json.contents.translated)
+        .catch(errorHandler);
 })
